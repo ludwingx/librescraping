3a11410920c415609adc1e6c1c605da1c7709cea
ludwingx <ludwingytx@gmail.com>
2025-08-04 15:16:23 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true, // Ignora errores de TypeScript durante el build
   },
+  images: {
+    domains: [
+      "noticias-admin-panel.vercel.app",
+      "tuto-noticias-test.vercel.app",
+      "i.postimg.cc",
+      "placeholder.com" 
+    ],
+  },
 };
 
 export default nextConfig;
